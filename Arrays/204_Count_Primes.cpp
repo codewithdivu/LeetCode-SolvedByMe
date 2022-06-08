0c55b85748f27bd4f672b31209e8299e7e6a1254
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-06-08 13:22:03 +0530

    added count primes in array

---
+++ Arrays/204_Count_Primes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        int cnt=0;
+        vector<bool> primes(n+1,true);
+        primes[0]=primes[1]=false;
+        
+        for(int i=2;i<n;i++){
+            if(primes[i]){
+                cnt++;
+                for(int j=2*i;j<n;j+=i){
+                    primes[j]=0;
+                }
+            }
+        }
+        return cnt;
+    }
+};
