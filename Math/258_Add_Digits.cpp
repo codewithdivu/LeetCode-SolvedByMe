7e500cac5a90f9470c2456c6d61a58a2d0d10c7a
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-06-13 23:41:00 +0530

    added 258 number question of MATH

---
+++ Math/258_Add_Digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum = 0;
+        if(num<10){
+            return num;
+        }
+        while(num>0)
+        {
+            int lastdigit = num%10;
+            sum = sum + lastdigit;
+            num = num/10;
+        }
+        num=sum;
+        // addDigits(num);
+        return addDigits(num);
+    }
+};
