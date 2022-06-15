e576d00db88cdc3cd188d90b2f1f2e27158bbeb1
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-06-15 23:56:43 +0530

    26 no added in array

---
+++ Arrays/26_Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k=1;
+        
+        vector<int>::iterator ptr1;
+        vector<int>::iterator ptr2;
+        ptr1=nums.begin();
+        ptr2=nums.begin()+1;
+        
+        
+        while(ptr2!=nums.end()){
+            if(*ptr1==*ptr2){
+                nums.erase(ptr2);
+            }
+            else{
+            ptr1++;
+            ptr2++;
+            }
+        }
+        return nums.size();
+    }
+};
