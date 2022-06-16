4f7ce40d917f95db583333401a5280caf2c64c6e
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-06-16 22:49:55 +0530

    917 added in STRING

---
+++ String/917_Reverse_Only_Letters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int start = 0;
+        int end = s.length()-1;
+        
+        while(start<end){
+            if(!isalpha(s[start])){
+                start++;
+            }
+            if(!isalpha(s[end])){
+                end--;
+            }
+            else if(isalpha(s[start]) && isalpha(s[end])){
+                swap(s[start],s[end]);
+                start++;
+                end--;
+            }
+        }
+        return s;
+    }
+};
