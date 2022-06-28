ceac77c0eba5ef8cc13582e45a147736540e7005
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-06-28 20:54:47 +0530

    24 is added in LINKED LIST

---
+++ LinkedList/24._Swap_Nodes_in_Pairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:   
+    ListNode* swapPairs(ListNode* head) {
+        
+        ListNode* temp = head;
+       
+        while(temp!=NULL && temp->next!=NULL){
+            swap(temp->val,temp->next->val);
+            temp=temp->next->next;
+        }
+        return head;
+    }
+};
