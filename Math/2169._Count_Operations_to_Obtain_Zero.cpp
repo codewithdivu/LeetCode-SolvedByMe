1d78750af26e103f59d4e4c011309ce9c30704c9
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-07-07 18:26:27 +0530

    2169 is added in MATH

---
+++ Math/2169._Count_Operations_to_Obtain_Zero.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        
+        int steps = 0;
+        
+        while(num1!=0 && num2!=0){
+            
+            if(num1>=num2){
+                num1 = num1 - num2;
+            }
+            else if(num2>num1){
+                num2 = num2 - num1;
+            }
+            steps++;
+        }
+        return steps;
+    }
+};
