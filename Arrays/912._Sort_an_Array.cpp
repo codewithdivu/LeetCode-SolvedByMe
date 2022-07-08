158ccaf082a379fbb3ac1d18b9cbb9a0041b4ffb
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-07-08 21:04:12 +0530

    912 is added in ARRAY

---
+++ Arrays/912._Sort_an_Array.cpp
@@ -0,0 +1,13 @@
+
+// we will cover it later with MERGE SORT
+
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        
+       sort(nums.begin(),nums.end());
+        
+        return nums;
+    }
+};
