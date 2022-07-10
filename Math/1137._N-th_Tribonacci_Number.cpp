4aa428565699968d3d9ff96adacf83c5e1a28639
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-07-10 11:33:33 +0530

    1137 is added in MATH

---
+++ Math/1137._N-th_Tribonacci_Number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        
+        if(n==0){
+            return 0;
+        }
+        if(n==1 || n==2){
+            return 1;
+        }
+        
+        int t1 = 0;
+        int t2 = 1;
+        int t3 = 1;
+        int temp;
+        for(int i=2;i<n;i++){
+            temp = t1 + t2 + t3;
+            cout<< temp << " ";
+            t1 = t2;
+            t2 = t3;
+            t3 = temp;
+        }
+        return t3;
+    }
+};
