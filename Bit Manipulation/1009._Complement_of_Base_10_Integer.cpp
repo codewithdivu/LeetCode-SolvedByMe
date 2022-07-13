ba886eab7bf9a743805d66f8f611c4bea5a68737
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-07-13 09:47:27 +0530

    i have solved 1009 with another solution

--- Bit Manipulation/1009._Complement_of_Base_10_Integer.cpp
+++ Bit Manipulation/1009._Complement_of_Base_10_Integer.cpp
@@ -12,3 +12,30 @@ public:
         return ans;
     }
 };
+
+
+
+// ----------------ANOTHER SOLUTION BY ME -----------------------
+
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+    int ans = 0;
+        int i=0;
+        if(n==0){
+            return 1;
+        }
+       while(n!=0){
+           
+           int bit = n&1;
+           
+           if(bit==0){
+               ans = (1*pow(2,i)) + ans;
+           }
+           i=i+1;
+           n=n>>1;
+           
+       }
+        return ans;
+    }
+};
