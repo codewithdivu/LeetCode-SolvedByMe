66d7693c2b8b7f41612bb7f1b86d9b0fc90c6032
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-07-18 11:36:37 +0530

    solved 852 with another logic

--- Arrays/852_Peak_Index_in_a_Mountain_Array.cpp
+++ Arrays/852_Peak_Index_in_a_Mountain_Array.cpp
@@ -1,23 +1,51 @@
+// class Solution {
+// public:
+//     int peakIndexInMountainArray(vector<int>& arr) 
+//     {
+        
+//         int start = 0;
+//         int end = arr.size();
+        
+//         while(start<end)
+//         {
+//             int mid = start + (end-start)/2;
+//             if(arr[mid]<arr[mid+1])
+//             {
+//                 start = mid+1;
+//             }
+//             else
+//             {
+//                 end = mid;
+//             }
+//         }
+//         return start;
+//     }
+// };
+
+
+// ...................MY SOLUTION ...............
+
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& arr) 
-    {
-        
-        int start = 0;
-        int end = arr.size();
-        
-        while(start<end)
-        {
-            int mid = start + (end-start)/2;
-            if(arr[mid]<arr[mid+1])
-            {
-                start = mid+1;
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int start  = 0;
+        int end = arr.size()-1;
+        int mid = start + (end-start)/2;
+        int ans;
+        while(start<=end){
+            mid = start + (end-start)/2;
+            
+            if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
+                ans = mid;
+                break;
             }
-            else
-            {
-                end = mid;
+            else if(arr[mid]<arr[mid+1]){
+                start = mid +1;
+            }
+            else{
+                end=mid-1;
             }
         }
-        return start;
+        return ans;
     }
 };
