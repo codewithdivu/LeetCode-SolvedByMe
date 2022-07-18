9fe6cc0f301f6b0d5079b7adce6240e9474557c9
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-07-18 14:28:32 +0530

    que no 69 is solved  using BINARY SEARCH by me

--- Math/69._Sqrt(x).cpp
+++ Math/69._Sqrt(x).cpp
@@ -1,6 +1,49 @@
+
+// we have done it using in-built function
+
 class Solution {
 public:
     int mySqrt(int x) {
         return sqrt(x);
+    }
+};
+
+
+// below solution is done by BINARY SEARCHt
+
+// time-complexicity -> o(log n)
+
+class Solution {
+private:
+    long long int binarySearch(int num){
+        int start = 0;
+        int end = num;
+        long long int mid = start + (end-start)/2;
+        long long int ans = -1;
+        
+        while(start<=end){
+            mid = start + (end-start)/2;
+            
+            long long int square = mid * mid;
+            
+            if(square==num){
+                return mid;
+            }
+            if(square>num){
+                end = mid -1;
+            }
+            else if(square<num){
+                ans = mid;
+                start = mid + 1;
+            }
+            
+        }
+        return ans;
+    }
+public:
+    int mySqrt(int x) {
+        
+        return binarySearch(x);
+        
     }
 };
