af227e46ea4acc8244f0120b263e80ca17a1ef7d
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-07-30 21:37:13 +0530

    74 is done in matrix

---
+++ Matrix/74._Search_a_2D_Matrix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        vector<int> ans;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[i].size();j++){
+                if(matrix[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
