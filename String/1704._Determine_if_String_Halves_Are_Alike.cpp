11e8da660af32e6b0551e1c396799ec8ea89c4e1
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-08-01 14:59:10 +0530

    some unuse code removed from question no 1704

--- String/1704._Determine_if_String_Halves_Are_Alike.cpp
+++ String/1704._Determine_if_String_Halves_Are_Alike.cpp
@@ -4,13 +4,10 @@ public:
     bool halvesAreAlike(string s)
     {
         int len = s.length();
-        string first = "";
-        string second = "";
         int firstCount = 0;
         int secondCount = 0;
         for (int i = 0; i <= len / 2 - 1; i++)
         {
-            first = first + s[i];
             if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
             {
                 firstCount += 1;
@@ -18,13 +15,11 @@ public:
         }
         for (int j = len / 2; j < len; j++)
         {
-            second = second + s[j];
             if (s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u' || s[j] == 'A' || s[j] == 'E' || s[j] == 'I' || s[j] == 'O' || s[j] == 'U')
             {
                 secondCount += 1;
             }
         }
-        cout << firstCount << " " << secondCount;
 
         if (firstCount == secondCount)
         {
