46a5db203fae67c778f03bc8058a5f72fb7a07f9
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-08-06 16:08:53 +0530

    second way of code and faster than above is added

--- Matrix/832._Flipping_an_Image.cpp
+++ Matrix/832._Flipping_an_Image.cpp
@@ -26,3 +26,49 @@ public:
         return image;
     }
 };
+
+
+/// second way ans it is faster than above
+
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        
+        
+        for(int i=0;i<image.size();i++){
+            int start = 0;
+            int end = image[i].size()-1;
+            while(start<=end){
+                swap(image[i][start],image[i][end]);
+                if(image[i][start]==0){
+                    image[i][start]=1;
+                }
+                else{
+                    image[i][start]=0;
+                }
+                if(start!=end){
+                if(image[i][end]==0){
+                    image[i][end]=1;
+                }
+                else{
+                    image[i][end]=0;
+                }
+                }
+                start++;
+                end--;
+            }
+        }
+                
+        // for(int m=0;m<image.size();m++){
+        //     for(int n=0;n<image[m].size();n++){
+        //         if(image[m][n]==0){
+        //             image[m][n]=1;
+        //         }
+        //         else{
+        //             image[m][n]=0;
+        //         }
+        //     }
+        // }
+        return image;
+    }
+};
