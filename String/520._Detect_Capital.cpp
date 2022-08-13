48b3e8549414149a8ab974ee9b5901fd1b5cdfb8
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-08-13 22:04:26 +0530

    520 is done

---
+++ String/520._Detect_Capital.cpp
@@ -0,0 +1,41 @@
+class Solution
+{
+public:
+    bool detectCapitalUse(string word)
+    {
+        if (word.size() == 1)
+            return 1;
+
+        if (word[0] >= 'A' && word[0] <= 'Z')
+        {
+            if (word[1] >= 'A' && word[1] <= 'Z')
+            {
+                for (int i = 2; i < word.size(); i++)
+                {
+                    if (word[i] >= 'a' && word[i] <= 'z')
+                        return false;
+                }
+            }
+
+            else if (word[1] >= 'a' && word[1] <= 'z')
+            {
+                for (int i = 2; i < word.size(); i++)
+                {
+                    if (word[i] >= 'A' && word[i] <= 'Z')
+                        return false;
+                }
+            }
+        }
+
+        else
+        {
+            for (int i = 1; i < word.size(); i++)
+            {
+                if (word[i] >= 'A' && word[i] <= 'Z')
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
