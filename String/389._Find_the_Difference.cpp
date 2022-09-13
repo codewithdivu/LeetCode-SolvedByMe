ea768691ddf67db3ca182c025bdffe38dac8268f
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-09-13 20:25:55 +0530

    .....

---
+++ String/389._Find_the_Difference.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    char findTheDifference(string s, string t)
+    {
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        char ans;
+
+        for (int i = 0; i < t.length(); i++)
+        {
+            if (t[i] != s[i])
+            {
+                ans = t[i];
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
