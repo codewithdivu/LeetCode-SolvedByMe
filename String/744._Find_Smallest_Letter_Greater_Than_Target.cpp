7ce7173580717396e7ee5c606110ca5540213485
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-10-26 20:53:08 +0530

    .....

---
+++ String/744._Find_Smallest_Letter_Greater_Than_Target.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        
+        char ans;
+        
+        for(int i=0;i<letters.size();i++){
+            if(letters[i]>target){
+                return letters[i];
+                break;
+            }
+        }
+        return letters[0];
+    }
+};
