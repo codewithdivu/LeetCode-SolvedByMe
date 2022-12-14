8b1eeee66b60f55cceec26716a61b17755dd36a1
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-12-14 20:53:29 +0530

    added 2 string question

---
+++ String/1844._Replace_All_Digits_with_Characters.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    string replaceDigits(string s)
+    {
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] >= '0' && s[i] <= '9')
+            {
+                int ll = s[i] - 48;
+                s[i] = s[i - 1] + ll;
+            }
+        }
+        return s;
+    }
+};
