e1040417bc69c4d6c04f59df70bfd68eee0c2fd9
codewithdivu <mavadiyadivyesh56@gmail.com>
2023-01-03 17:38:07 +0530

    formatted

--- Bit Manipulation/1017._Convert_to_Base_-2.cpp
+++ Bit Manipulation/1017._Convert_to_Base_-2.cpp
@@ -1,15 +1,19 @@
-class Solution {
+class Solution
+{
 public:
-    string baseNeg2(int n) {
+    string baseNeg2(int n)
+    {
         string ans = "";
-        
-        while(n!=0){
-            int digit = n&1;
+
+        while (n != 0)
+        {
+            int digit = n & 1;
             ans = to_string(digit) + ans;
-            n = -(n>>1);
+            n = -(n >> 1);
         }
-        
-        if(ans == ""){
+
+        if (ans == "")
+        {
             return "0";
         }
         return ans;
