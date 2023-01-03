a2e733af2c61afe79d38b87887a7ab17e8999455
codewithdivu <mavadiyadivyesh56@gmail.com>
2023-01-03 17:37:24 +0530

    it's simple done using BIT MANIPULATION

---
+++ Bit Manipulation/1017._Convert_to_Base_-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string baseNeg2(int n) {
+        string ans = "";
+        
+        while(n!=0){
+            int digit = n&1;
+            ans = to_string(digit) + ans;
+            n = -(n>>1);
+        }
+        
+        if(ans == ""){
+            return "0";
+        }
+        return ans;
+    }
+};
