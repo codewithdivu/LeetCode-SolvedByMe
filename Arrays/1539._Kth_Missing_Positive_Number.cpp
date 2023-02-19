fb5216275dd377a753bc6b35cc94155d06182270
codewithdivu <mavadiyadivyesh56@gmail.com>
2023-02-19 22:02:22 +0530

    done using binary search

---
+++ Arrays/1539._Kth_Missing_Positive_Number.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    int findKthPositive(vector<int> &arr, int k)
+    {
+        int start = 0;
+        int end = arr.size();
+
+        while (start < end)
+        {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] - mid - 1 < k)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid;
+            }
+        }
+        return start + k;
+    }
+};
